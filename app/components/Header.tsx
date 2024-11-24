a99457831f319dab32ca0ec038f33af97582ccde
imagion <algernoncowperwood@gmail.com>
2024-11-24 09:07:52 +0300

    removed unused components

--- app/components/Header.tsx
+++
@@ -1,3 +0,0 @@
-export default function Header() {
-  return <h1 className='text-2xl text-neutral-600 text-center'>Weather</h1>;
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,8 @@
-// 'use client';
-
-// import Header from '@/app/components/Header';
-// import Form from '@/app/components/Form';
-// import Card from '@/app/components/Card';
-// import { useState } from 'react';
 import Weather from '@/app/components/Weather';
 
 export default function Home() {
-  // const [city, setCity] = useState('');
-
   return (
     <div className='w-[min(100%,25rem)] flex flex-col gap-3 bg-neutral-200 shadow-xl p-6 mt-10'>
-      {/* <Header />
-      <Form setCity={setCity} />
-      {city && <Card city={city} />} */}
       <Weather />
     </div>
   );
